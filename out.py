7f9d45193406a14ce49e18764b86d9e2314b98e1
h1am1a <duykhanhshop81@gmail.com>
2025-03-28 22:47:16 +0700

    Add files via upload

---
+++ out.py
@@ -0,0 +1,6 @@
+import os,sys
+import requests,json
+from time import sleep
+from datetime import datetime, timedelta
+import base64,requests,os
+print("[</>] => Cảm Bạn Đã sử dụng Tool Nhé! chúc bạn một ngày vui 🥰")
